0f1e767d03f0ff0276242d5f0a745be7137b78f6
Carl Stephens <carlst99@outlook.co.nz>
2020-11-25 22:09:41 +1300

    Add project files.

---
+++ UVOCBot/Commands/TwitterModule.cs
@@ -0,0 +1,13 @@
+﻿using DSharpPlus.CommandsNext;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UVOCBot.Commands
+{
+    public class TwitterModule : BaseCommandModule
+    {
+        //public async Task PostChannel
+    }
+}
---
+++ UVOCBot/Model/GuildSettings.cs
@@ -0,0 +1,12 @@
+﻿using Realms;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UVOCBot.Model
+{
+    public class GuildSettings : RealmObject
+    {
+        public int Id { get; set; }
+    }
+}

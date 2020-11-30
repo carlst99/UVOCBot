681ffd57d6b160dbafb361054b1059949d08136a
Carl Stephens <carlst99@outlook.co.nz>
2020-12-01 00:40:06 +1300

    Migrate to sqlite db

--- UVOCBot/Model/GuildTwitterSettings.cs
+++ UVOCBot/Model/GuildTwitterSettings.cs
@@ -1,26 +1,27 @@
-﻿using Realms;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace UVOCBot.Model
 {
     /// <summary>
     /// Contains guild-specific settings regarding their twitter preferences
     /// </summary>
-    public sealed class GuildTwitterSettings : RealmObject
+    public sealed class GuildTwitterSettings
     {
+        public int Id { get; set; }
+
         /// <summary>
         /// Gets or sets the guild that these Twitter settings are for
         /// </summary>
-        public ulong GuildId { get; set; }
+        public GuildSettings Guild { get; set; }
 
         /// <summary>
-        /// Gets or sets the guild channel in which Twitter posts should be relayed to
+        /// Gets or sets the Discord id of the guild channel in which Twitter posts should be relayed to
         /// </summary>
         public ulong RelayChannelId { get; set; }
 
         /// <summary>
-        /// Gets or sets a list of users from whom to relay Twitter posts
+        /// Gets or sets a list of twitter user ids from whom to relay posts
         /// </summary>
-        public IList<long> TwitterUserIds { get; }
+        public List<long> TwitterUserIds { get; }
     }
 }

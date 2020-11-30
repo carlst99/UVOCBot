50e9a6a8f008316aa85e68febc86e1bba48ab9f6
Carl Stephens <carlst99@outlook.co.nz>
2020-11-30 23:50:58 +1300

    Complete TwitterService

---
+++ UVOCBot/JobRegistry.cs
@@ -0,0 +1,13 @@
+﻿using FluentScheduler;
+using UVOCBot.Services;
+
+namespace UVOCBot
+{
+    public class JobRegistry : Registry
+    {
+        public JobRegistry()
+        {
+            Schedule<TwitterService>().ToRunNow().AndEvery(10).Minutes();
+        }
+    }
+}
---
+++ UVOCBot/Model/GuildTwitterSettings.cs
@@ -0,0 +1,26 @@
+﻿using Realms;
+using System.Collections.Generic;
+
+namespace UVOCBot.Model
+{
+    /// <summary>
+    /// Contains guild-specific settings regarding their twitter preferences
+    /// </summary>
+    public sealed class GuildTwitterSettings : RealmObject
+    {
+        /// <summary>
+        /// Gets or sets the guild that these Twitter settings are for
+        /// </summary>
+        public ulong GuildId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the guild channel in which Twitter posts should be relayed to
+        /// </summary>
+        public ulong RelayChannelId { get; set; }
+
+        /// <summary>
+        /// Gets or sets a list of users from whom to relay Twitter posts
+        /// </summary>
+        public IList<long> TwitterUserIds { get; }
+    }
+}

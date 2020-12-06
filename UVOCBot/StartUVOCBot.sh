919df8c960f0dab6479f1349e30cc7b297aaa328
Carl Stephens <carlst99@outlook.co.nz>
2020-12-06 19:01:34 +1300

    Add linux startup script

---
+++ UVOCBot/StartUVOCBot.sh
@@ -0,0 +1,14 @@
+﻿#!/bin/bash
+
+export DOTNET_ROOT="path/to/your/dotnet/installation"
+export UVOCBOT_DB_PASSWD="The password for your database user"
+export UVOCBOT_TWITTERAPI_KEY="Your Twitter API key"
+export UVOCBOT_DB_USER="The user for your database"
+export UVOCBOT_BOT_TOKEN="Your Discord application Bot Token"
+export DOTNET_ENVIRONMENT="Release"
+export UVOCBOT_TWITTERAPI_SECRET="Your Twitter API secret"
+export UVOCBOT_DB_NAME="The name of your database"
+export UVOCBOT_DB_SERVER="The address of your database server"
+export UVOCBOT_TWITTERAPI_BEARER_TOKEN="Your Twitter API bearer token"
+
+./UVOCBot

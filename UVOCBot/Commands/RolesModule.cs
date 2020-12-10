0d98cbe27127ba8ef2a8199a490bad754cc0d781
Carl Stephens <carlst99@outlook.co.nz>
2020-12-10 17:39:52 +1300

    Implement Roles->AddByReactionCommand

--- UVOCBot/Commands/RolesModule.cs
+++ UVOCBot/Commands/RolesModule.cs
@@ -2,7 +2,12 @@
 using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
 using DSharpPlus.Entities;
+using DSharpPlus.Exceptions;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using UVOCBot.Extensions;
 
 namespace UVOCBot.Commands
 {
@@ -11,6 +16,7 @@ namespace UVOCBot.Commands
     [Description("Commands pertinent to role management")]
     [ModuleLifespan(ModuleLifespan.Transient)]
     [RequireUserPermissions(Permissions.ManageRoles)]
+    [Hidden]
     public class RolesModule : BaseCommandModule
     {
         [Command("add-by-reaction")]
@@ -25,12 +31,58 @@ namespace UVOCBot.Commands
         {
             await ctx.TriggerTypingAsync().ConfigureAwait(false);
 
-            DiscordMessage message = await channel.GetMessageAsync(messageId).ConfigureAwait(false);
-            if (message is null)
+            // Attempt to find the message
+            DiscordMessage message;
+            try
+            {
+                message = await channel.GetMessageAsync(messageId).ConfigureAwait(false);
+            }
+            catch (NotFoundException)
             {
                 await ctx.RespondAsync("Could not get the provided message. Please ensure you copied the right ID, and that I have permissions to view the channel that the message was posted in").ConfigureAwait(false);
                 return;
             }
+
+            // Check that the message has the specified reaction emoji
+            bool reactionFound = false;
+            int userCount = 0;
+            foreach (DiscordReaction reaction in message.Reactions)
+            {
+                if (reaction.Emoji.Equals(emoji))
+                {
+                    reactionFound = true;
+                    userCount = reaction.Count;
+                    break;
+                }
+            }
+            if (!reactionFound)
+            {
+                await ctx.RespondAsync("The provided message didn't have any reactions with that emoji").ConfigureAwait(false);
+                return;
+            }
+
+            // Add the role to users who don't have it
+            IReadOnlyList<DiscordUser> users = await message.GetReactionsAsync(emoji, userCount).ConfigureAwait(false);
+            StringBuilder responseBuilder = new StringBuilder("The role **").Append(role.Name).AppendLine("** was granted to the following users:");
+            foreach (DiscordUser user in users)
+            {
+                DiscordMember member = await ctx.Guild.GetMemberAsync(user.Id).ConfigureAwait(false);
+                if (!member.Roles.Contains(role) && await GrantRoleToUserAsync(ctx, role, user).ConfigureAwait(false))
+                    responseBuilder.Append(member.GetFriendlyName()).Append(", ");
+            }
+
+            await ctx.RespondAsync(responseBuilder.ToString().Trim(',', ' ')).ConfigureAwait(false);
+        }
+
+        private static async Task<bool> GrantRoleToUserAsync(CommandContext ctx, DiscordRole role, DiscordUser user)
+        {
+            DiscordMember member = await ctx.Guild.GetMemberAsync(user.Id).ConfigureAwait(false);
+            if (!member.Roles.Contains(role))
+            {
+                await member.GrantRoleAsync(role, $"Role grant requested by {ctx.User.Username}").ConfigureAwait(false);
+                return true;
+            }
+            return false;
         }
     }
 }

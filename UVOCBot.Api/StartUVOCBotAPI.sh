30a4cabe5d7724a72d2bb46f6852019c8d61c431
Carl Stephens <carlst99@outlook.co.nz>
2020-12-22 18:36:33 +1300

    Update service/startup files

---
+++ UVOCBot.Api/StartUVOCBotAPI.sh
@@ -0,0 +1,12 @@
+﻿#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+export DOTNET_ROOT="path/to/your/dotnet/installation"
+export UVOCBOT_DB_PASSWD="The password for your database user"
+export UVOCBOT_DB_USER="The user for your database"
+export DOTNET_ENVIRONMENT="Release"
+export UVOCBOT_DB_NAME="The name of your database"
+export UVOCBOT_DB_SERVER="The address of your database server"
+
+$DIR/UVOCBot

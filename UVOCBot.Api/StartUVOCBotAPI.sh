30590328fb960e8bc20bd494ef6e88ae2db7c79b
Carl Stephens <carlst99@outlook.co.nz>
2020-12-23 14:24:41 +1300

    Update startup scripts

--- UVOCBot.Api/StartUVOCBotAPI.sh
+++ UVOCBot.Api/StartUVOCBotAPI.sh
@@ -1,12 +1,13 @@
-﻿#!/bin/bash
+#!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 export DOTNET_ROOT="path/to/your/dotnet/installation"
-export UVOCBOT_DB_PASSWD="The password for your database user"
-export UVOCBOT_DB_USER="The user for your database"
-export DOTNET_ENVIRONMENT="Release"
-export UVOCBOT_DB_NAME="The name of your database"
-export UVOCBOT_DB_SERVER="The address of your database server"
+export ASPNETCORE_ENVIRONMENT="Release"
+export ASPNETCORE_URLS="http://localhost:52728"
+export UVOCBOTAPI_DB_PASSWD="The password for your database user"
+export UVOCBOTAPI_DB_USER="The user for your database"
+export UVOCBOTAPI_DB_NAME="The name of your database"
+export UVOCBOTAPI_DB_SERVER="The address of your database server"
 
-$DIR/UVOCBot
+$DIR/UVOCBot.Api

6f77415efe2cd04975d0796c0113a0d8df922f38
Carl Stephens <carlst99@outlook.co.nz>
2021-04-07 12:43:23 +1200

    Add move-group command

--- UVOCBotRemora/Commands/MovementCommands.cs
+++ UVOCBotRemora/Commands/MovementCommands.cs
@@ -2,6 +2,7 @@
 using Remora.Commands.Groups;
 using Remora.Discord.API.Abstractions.Objects;
 using Remora.Discord.API.Abstractions.Rest;
+using Remora.Discord.Commands.Attributes;
 using Remora.Discord.Commands.Conditions;
 using Remora.Discord.Commands.Contexts;
 using Remora.Discord.Core;
@@ -9,7 +10,9 @@ using Remora.Results;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
 using System.Threading.Tasks;
+using UVOCBot.Core.Model;
 using UVOCBotRemora.Services;
 
 namespace UVOCBotRemora.Commands
@@ -34,7 +37,7 @@ namespace UVOCBotRemora.Commands
         }
 
         [Command("move")]
-        [Description("Moves everyone from the voice channel the command caller is currently connected to to another")]
+        [Description("Moves a group from one voice channel to another")]
         public async Task<IResult> MoveCommandAsync(
             [Description("The voice channel to move people to")] IChannel moveTo,
             [Description("An optional voice channel to move people from")] IChannel? moveFrom = null)
@@ -42,7 +45,7 @@ namespace UVOCBotRemora.Commands
             if (moveTo.Type != ChannelType.GuildVoice)
             {
                 await _responder.RespondWithErrorAsync(_context, $"Please specify a valid {Formatter.Bold("voice")} channel to move to.", ct: CancellationToken).ConfigureAwait(false);
-                return Result<Snowflake>.FromError(new InvalidOperationException());
+                return Result.FromSuccess();
             }
 
             Result<Snowflake> moveFromID = await GetMoveFromChannelIdAsync(moveFrom).ConfigureAwait(false);
@@ -55,6 +58,40 @@ namespace UVOCBotRemora.Commands
 
             foreach (IVoiceState voiceState in voiceStates.Value)
                 await _guildAPI.ModifyGuildMemberAsync(_context.GuildID.Value, voiceState.UserID, channelID: moveTo.ID, ct: CancellationToken).ConfigureAwait(false);
+
+            return await _responder.RespondWithSuccessAsync(_context, Formatter.Emoji("white_check_mark"), ct: CancellationToken).ConfigureAwait(false);
+        }
+
+        [Command("move-group")]
+        [Description("Moves a group from one voice channel to another")]
+        public async Task<IResult> MoveGroupCommandAsync(
+            [Description("The member group to move. See the group commands for more info")][DiscordTypeHint(TypeHint.String)] string groupName,
+            [Description("The voice channel to move people to")] IChannel moveTo,
+            [Description("An optional voice channel to move people from")] IChannel? moveFrom = null)
+        {
+            if (moveTo.Type != ChannelType.GuildVoice)
+            {
+                await _responder.RespondWithErrorAsync(_context, $"Please specify a valid {Formatter.Bold("voice")} channel to move to.", ct: CancellationToken).ConfigureAwait(false);
+                return Result.FromSuccess();
+            }
+
+            Result<Snowflake> moveFromID = await GetMoveFromChannelIdAsync(moveFrom).ConfigureAwait(false);
+            if (!moveFromID.IsSuccess)
+                return Result.FromSuccess();
+
+            Result<IReadOnlyList<Snowflake>> groupMembers = await GetGroupMembersAsync(groupName).ConfigureAwait(false);
+            if (!groupMembers.IsSuccess)
+                return Result.FromSuccess(); // Not our error if the group cannot be found
+
+            Optional<IReadOnlyList<IVoiceState>> voiceStates = _voiceStateCache.GetChannelVoiceStates(moveFromID.Entity);
+            if (!voiceStates.HasValue)
+                return await _responder.RespondWithErrorAsync(_context, "There are no members to move", ct: CancellationToken).ConfigureAwait(false);
+
+            foreach (IVoiceState voiceState in voiceStates.Value)
+            {
+                if (groupMembers.Entity.Contains(voiceState.UserID))
+                    await _guildAPI.ModifyGuildMemberAsync(_context.GuildID.Value, voiceState.UserID, channelID: moveTo.ID, ct: CancellationToken).ConfigureAwait(false);
+            }
 
             return await _responder.RespondWithSuccessAsync(_context, Formatter.Emoji("white_check_mark"), ct: CancellationToken).ConfigureAwait(false);
         }
@@ -83,6 +120,20 @@ namespace UVOCBotRemora.Commands
                 }
 
                 return moveFromChannel.ID;
+            }
+        }
+
+        private async Task<Result<IReadOnlyList<Snowflake>>> GetGroupMembersAsync(string groupName)
+        {
+            try
+            {
+                MemberGroupDTO group = await _dbAPI.GetMemberGroup(_context.GuildID.Value.Value, groupName).ConfigureAwait(false);
+                return group.UserIds.ConvertAll(i => new Snowflake(i)).AsReadOnly();
+            }
+            catch (Refit.ValidationApiException vaex) when (vaex.StatusCode == System.Net.HttpStatusCode.NotFound)
+            {
+                await _responder.RespondWithErrorAsync(_context, "That group doesn't exist.", ct: CancellationToken).ConfigureAwait(false);
+                return Result<IReadOnlyList<Snowflake>>.FromError<ArgumentException>(new ArgumentException("Invalid group name"));
             }
         }
     }

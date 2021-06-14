0508fb2c0c6d6cbb86820a4aef58252c62e92e0e
Carl Stephens <carlst99@outlook.co.nz>
2021-06-14 13:52:58 +1200

    Add GuildWelcomeMessage endpoints to DbApiService

--- UVOCBot/Services/DbAPIService.cs
+++ UVOCBot/Services/DbAPIService.cs
@@ -304,5 +304,43 @@ namespace UVOCBot.Services
         }
 
         #endregion
+
+        #region GuildWelcomeMessage
+
+        public async Task<Result<GuildWelcomeMessageDto>> GetGuildWelcomeMessageAsync(ulong id, CancellationToken ct = default)
+        {
+            IRestRequest request = new RestRequest("guildwelcomemessage/{id}", Method.GET);
+            request.AddParameter("id", id, ParameterType.UrlSegment);
+
+            return await ExecuteAsync<GuildWelcomeMessageDto>(request, ct).ConfigureAwait(false);
+        }
+
+        public async Task<Result> UpdateGuildWelcomeMessageAsync(ulong id, GuildWelcomeMessageDto settings, CancellationToken ct = default)
+        {
+            IRestRequest request = new RestRequest("guildwelcomemessage/{id}", Method.PUT);
+            request.AddParameter("id", id, ParameterType.UrlSegment);
+
+            request.AddJsonBody(settings);
+
+            return await ExecuteAsync(request, ct).ConfigureAwait(false);
+        }
+
+        public async Task<Result<GuildWelcomeMessageDto>> CreateGuildSettingsAsync(GuildWelcomeMessageDto settings, CancellationToken ct = default)
+        {
+            IRestRequest request = new RestRequest("guildwelcomemessage", Method.POST);
+            request.AddJsonBody(settings);
+
+            return await ExecuteAsync<GuildWelcomeMessageDto>(request, ct).ConfigureAwait(false);
+        }
+
+        public async Task<Result> DeleteGuildWelcomeMessageAsync(ulong id, CancellationToken ct = default)
+        {
+            IRestRequest request = new RestRequest("guildwelcomemessage/{id}", Method.DELETE);
+            request.AddParameter("id", id, ParameterType.UrlSegment);
+
+            return await ExecuteAsync(request, ct).ConfigureAwait(false);
+        }
+
+        #endregion
     }
 }

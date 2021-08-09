d19b23f836f3cc5e9e4ed6e0fe3ce18da37d90e9
Carl Stephens <carlst99@outlook.co.nz>
2021-08-09 22:00:00 +1200

    Re-add migrations. Close call!

---
+++ UVOCBot.Core/Migrations/20210107093604_AddBonkChannelId.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace UVOCBot.Core.Migrations
+{
+    public partial class AddBonkChannelId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<ulong>(
+                name: "BonkChannelId",
+                table: "GuildSettings",
+                type: "bigint unsigned",
+                nullable: true,
+                defaultValue: 0ul);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BonkChannelId",
+                table: "GuildSettings");
+        }
+    }
+}

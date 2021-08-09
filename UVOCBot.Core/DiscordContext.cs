ba8504bc6d8e9ff932eb2daf3fa75bc23961201c
Carl Stephens <carlst99@outlook.co.nz>
2021-08-09 21:53:48 +1200

    Add janky migration constructor

--- UVOCBot.Core/DiscordContext.cs
+++ UVOCBot.Core/DiscordContext.cs
@@ -17,11 +17,23 @@ namespace UVOCBot.Core
         public DbSet<MemberGroup> MemberGroups { get; set; }
 
 #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+        /// <summary>
+        /// Janky hardcoded constructor for generating migrations.
+        /// </summary>
+        public DiscordContext()
+        {
+            _config = new DatabaseOptions
+            {
+                ConnectionString = "server = localhost; user = uvocbot_test; database = db_uvocbot_test",
+                DatabaseVersion = "10.5.8"
+            };
+        }
+
         public DiscordContext(IOptions<DatabaseOptions> config)
-#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
         {
             _config = config.Value;
         }
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 
         protected override void OnConfiguring(DbContextOptionsBuilder options)
         {

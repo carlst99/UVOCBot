d537f43725950bd7b6698496b9e69b20adbfd711
Carl Stephens <carlst99@outlook.co.nz>
2021-09-05 22:01:52 +1200

    Refactor CleanupWorker

--- UVOCBot.Api/Startup.cs
+++ UVOCBot.Api/Startup.cs
@@ -6,7 +6,6 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 using System;
-using UVOCBot.Api.Workers;
 using UVOCBot.Core;
 
 namespace UVOCBot.Api
@@ -44,7 +43,6 @@ namespace UVOCBot.Api
 
             services.AddControllers();
             services.AddSwaggerGen(c => c.SwaggerDoc("v1", new OpenApiInfo { Title = "UVOCBot.Api", Version = "v1" }));
-            services.AddHostedService<CleanupWorker>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

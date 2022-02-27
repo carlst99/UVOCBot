c4e0df6012e69c87613acea9b8822d7ec4e249e1
Carl Stephens <carlst99@outlook.co.nz>
2022-02-28 00:39:45 +1300

    Add message response functions to the InteractionResponseService

--- UVOCBot.Discord.Core/Abstractions/Services/IInteractionResponseService.cs
+++ UVOCBot.Discord.Core/Abstractions/Services/IInteractionResponseService.cs
@@ -1,5 +1,10 @@
+using OneOf;
+using Remora.Discord.API.Abstractions.Objects;
+using Remora.Discord.API.Abstractions.Rest;
 using Remora.Discord.API.Objects;
+using Remora.Rest.Core;
 using Remora.Results;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -18,8 +23,54 @@ public interface IInteractionResponseService
     /// <summary>
     /// Creates a modal interaction response.
     /// </summary>
-    /// <param name="modalData">The modal to submit.</param>
+    /// <param name="modalData">The modal to respond with.</param>
     /// <param name="ct">A <see cref="CancellationToken"/> that can be used to stop the operation.</param>
     /// <returns>A result representing the outcome of the operation.</returns>
-    Task<Result> CreateModalResponse(InteractionModalCallbackData modalData, CancellationToken ct = default);
+    Task<Result> CreateModalResponse
+    (
+        InteractionModalCallbackData modalData,
+        CancellationToken ct = default
+    );
+
+    /// <summary>
+    /// Creates a message interaction response.
+    /// </summary>
+    /// <param name="message">The message to response with.</param>
+    /// <param name="attachments">The attachments to send with the message.</param>
+    /// <param name="ct">A <see cref="CancellationToken"/> that can be used to stop the operation.</param>
+    /// <returns>A result representing the outcome of the operation.</returns>
+    Task<Result> CreateMessageResponse
+    (
+        InteractionMessageCallbackData message,
+        Optional<IReadOnlyList<OneOf<FileData, IPartialAttachment>>> attachments = default,
+        CancellationToken ct = default
+    );
+
+    /// <summary>
+    /// Creates a deferred message response. Use <see cref="CreateContextualMessageResponse"/>
+    /// or similar means to send followup messages.
+    /// </summary>
+    /// <param name="isEphemeral">A value indicating whether the deferred response should be ephemeral.</param>
+    /// <param name="ct">A <see cref="CancellationToken"/> that can be used to stop the operation.</param>
+    /// <returns>A result representing the outcome of the operation.</returns>
+    Task<Result> CreateDeferredMessageResponse
+    (
+        bool isEphemeral,
+        CancellationToken ct = default
+    );
+
+    /// <summary>
+    /// Creates a message interaction response. If an interaction response has already been
+    /// created for the scoped token, a followup message will be sent instead.
+    /// </summary>
+    /// <param name="message">The message to response with.</param>
+    /// <param name="attachments">The attachments to send with the message.</param>
+    /// <param name="ct">A <see cref="CancellationToken"/> that can be used to stop the operation.</param>
+    /// <returns>A result representing the outcome of the operation.</returns>
+    Task<Result> CreateContextualMessageResponse
+    (
+        InteractionMessageCallbackData message,
+        Optional<IReadOnlyList<OneOf<FileData, IPartialAttachment>>> attachments = default,
+        CancellationToken ct = default
+    );
 }
--- UVOCBot.Discord.Core/Services/InteractionResponseService.cs
+++ UVOCBot.Discord.Core/Services/InteractionResponseService.cs
@@ -5,8 +5,10 @@ using Remora.Discord.API.Objects;
 using Remora.Discord.Commands.Contexts;
 using Remora.Rest.Core;
 using Remora.Results;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
+using UVOCBot.Core;
 using UVOCBot.Discord.Core.Abstractions.Services;
 
 namespace UVOCBot.Discord.Core.Services;
@@ -47,8 +49,7 @@ public class InteractionResponseService : IInteractionResponseService
             new InteractionResponse
             (
                 InteractionCallbackType.Modal,
-                new Optional<OneOf<IInteractionMessageCallbackData, IInteractionAutocompleteCallbackData,
-                    IInteractionModalCallbackData>>(modalData)
+                new Optional<OneOf<IInteractionMessageCallbackData, IInteractionAutocompleteCallbackData, IInteractionModalCallbackData>>(modalData)
             ),
             ct: ct
         );
@@ -58,6 +59,99 @@ public class InteractionResponseService : IInteractionResponseService
 
         return responseResult;
     }
-    
-    
+
+    /// <inheritdoc />
+    public async Task<Result> CreateMessageResponse
+    (
+        InteractionMessageCallbackData message,
+        Optional<IReadOnlyList<OneOf<FileData, IPartialAttachment>>> attachments = default,
+        CancellationToken ct = default
+    )
+    {
+        if (HasResponded)
+            return new InvalidOperationError("A response has already been created for this scope.");
+
+        Result responseResult = await _interactionApi.CreateInteractionResponseAsync
+        (
+            _context.ID,
+            _context.Token,
+            new InteractionResponse
+            (
+                InteractionCallbackType.DeferredChannelMessageWithSource,
+                new Optional<OneOf<IInteractionMessageCallbackData, IInteractionAutocompleteCallbackData, IInteractionModalCallbackData>>(message)
+            ),
+            attachments,
+            ct
+        );
+
+        if (responseResult.IsSuccess)
+            HasResponded = true;
+
+        return responseResult;
+    }
+
+    /// <inheritdoc />
+    public async Task<Result> CreateDeferredMessageResponse
+    (
+        bool isEphemeral,
+        CancellationToken ct = default
+    )
+    {
+        if (HasResponded)
+            return new InvalidOperationError("A response has already been created for this scope.");
+
+        Optional<MessageFlags> flags = isEphemeral
+            ? MessageFlags.Ephemeral
+            : default;
+
+        Result responseResult = await _interactionApi.CreateInteractionResponseAsync
+        (
+            _context.ID,
+            _context.Token,
+            new InteractionResponse
+            (
+                InteractionCallbackType.DeferredChannelMessageWithSource,
+                new Optional<OneOf<IInteractionMessageCallbackData, IInteractionAutocompleteCallbackData, IInteractionModalCallbackData>>
+                (
+                    new InteractionMessageCallbackData(Flags: flags)
+                )
+            ),
+            ct: ct
+        );
+
+        if (responseResult.IsSuccess)
+            HasResponded = true;
+
+        return responseResult;
+    }
+
+    /// <inheritdoc />
+    public async Task<Result> CreateContextualMessageResponse
+    (
+        InteractionMessageCallbackData message,
+        Optional<IReadOnlyList<OneOf<FileData, IPartialAttachment>>> attachments = default,
+        CancellationToken ct = default
+    )
+    {
+        if (!HasResponded)
+            return await CreateMessageResponse(message, attachments, ct);
+
+        Result<IMessage> responseResult = await _interactionApi.CreateFollowupMessageAsync
+        (
+            DiscordConstants.ApplicationId,
+            _context.Token,
+            message.Content,
+            message.IsTTS,
+            message.Embeds,
+            message.AllowedMentions,
+            message.Components,
+            attachments,
+            message.Flags,
+            ct
+        );
+
+        return responseResult.IsSuccess
+            ? Result.FromSuccess()
+            : Result.FromError(responseResult);
+    }
 }

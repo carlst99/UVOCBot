59ff3eaacaa1673c7d358b9d55f6d4c8c59c4889
Carl <carlst99@outlook.co.nz>
2025-05-12 18:57:07 +1200

    Cleanup

--- UVOCBot.Core/Extensions/TaskExtensions.cs
+++ UVOCBot.Core/Extensions/TaskExtensions.cs
@@ -1,29 +1,30 @@
-﻿namespace System.Threading.Tasks;
+﻿// ReSharper disable once CheckNamespace
+namespace System.Threading.Tasks;
 
 public static class TaskExtensions
 {
     public static async Task WithCancellation(this Task task, CancellationToken cancellationToken = default)
     {
-        if (cancellationToken == default)
-            await task.ConfigureAwait(false);
+        if (cancellationToken == CancellationToken.None)
+            await task;
 
         TaskCompletionSource<bool> tcs = new();
         using (cancellationToken.Register(s => ((TaskCompletionSource<bool>)s!).TrySetResult(true), tcs))
         {
-            if (task != await Task.WhenAny(task, tcs.Task).ConfigureAwait(false))
+            if (task != await Task.WhenAny(task, tcs.Task))
                 throw new OperationCanceledException(cancellationToken);
         }
     }
 
     public static async Task<T> WithCancellation<T>(this Task<T> task, CancellationToken cancellationToken = default)
     {
-        if (cancellationToken == default)
-            return await task.ConfigureAwait(false);
+        if (cancellationToken == CancellationToken.None)
+            return await task;
 
         TaskCompletionSource<bool> tcs = new();
         using (cancellationToken.Register(s => ((TaskCompletionSource<bool>)s!).TrySetResult(true), tcs))
         {
-            if (task != await Task.WhenAny(task, tcs.Task).ConfigureAwait(false))
+            if (task != await Task.WhenAny(task, tcs.Task))
                 throw new OperationCanceledException(cancellationToken);
         }
 

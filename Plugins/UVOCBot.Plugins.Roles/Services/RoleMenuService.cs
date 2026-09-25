80acc25e5412f349b62072b3ba61e5074a1aa130
Carl <carlst99@outlook.co.nz>
2026-09-25 22:33:15 +1200

    chore: improved error detection for emoji issues in RM component posting

--- Plugins/UVOCBot.Plugins.Roles/Services/RoleMenuService.cs
+++ Plugins/UVOCBot.Plugins.Roles/Services/RoleMenuService.cs
@@ -6,6 +6,8 @@ using Remora.Discord.API.Abstractions.Objects;
 using Remora.Discord.API.Abstractions.Rest;
 using Remora.Discord.API.Abstractions.Results;
 using Remora.Discord.API.Objects;
+using Remora.Discord.Commands.Feedback.Messages;
+using Remora.Discord.Commands.Feedback.Services;
 using Remora.Rest.Core;
 using Remora.Rest.Results;
 using Remora.Results;
@@ -28,21 +30,21 @@ public class RoleMenuService : IRoleMenuService
 {
     private readonly ILogger<RoleMenuService> _logger;
     private readonly IDiscordRestChannelAPI _channelApi;
-    private readonly IDiscordRestEmojiAPI _emojiApi;
     private readonly DiscordContext _dbContext;
+    private readonly IFeedbackService _feedbackService;
 
     public RoleMenuService
     (
         ILogger<RoleMenuService> logger,
         IDiscordRestChannelAPI channelApi,
-        IDiscordRestEmojiAPI emojiApi,
-        DiscordContext dbContext
+        DiscordContext dbContext,
+        IFeedbackService feedbackService
     )
     {
         _logger = logger;
         _channelApi = channelApi;
-        _emojiApi = emojiApi;
         _dbContext = dbContext;
+        _feedbackService = feedbackService;
     }
 
     /// <inheritdoc />
@@ -80,9 +82,9 @@ public class RoleMenuService : IRoleMenuService
         (
             (r1, r2) => string.Compare(r1.Label, r2.Label, StringComparison.Ordinal)
         );
-        List<IMessageComponent> components = await CreateRoleMenuMessageComponents(menu, ct);
+        List<IMessageComponent> components = CreateRoleMenuMessageComponents(menu);
 
-        Result<IMessage> editResult = await _channelApi.EditMessageAsync
+        Result<IMessage> upsertResult = await _channelApi.EditMessageAsync
         (
             DiscordSnowflake.New(menu.ChannelId),
             DiscordSnowflake.New(menu.MessageId),
@@ -94,80 +96,126 @@ public class RoleMenuService : IRoleMenuService
         // If we couldn't edit the message, it's quite possible it was deleted. Let's check if that was the case
         // and attempt to recreate the message if so
 
-        if (editResult.IsSuccess || !restoreDeletedMenus)
-            return editResult;
+        if (upsertResult.IsSuccess || !restoreDeletedMenus)
+            return upsertResult;
 
         // Test for an UnknownMessage error - meaning we should recreate the message
-        bool failure = editResult.Error is not RestResultError<RestError> restError
+        bool failure = upsertResult.Error is not RestResultError<RestError> restError
             || !restError.Error.Code.TryGet(out DiscordError discordError)
             || discordError is not DiscordError.UnknownMessage;
 
         if (failure)
-        {
-            _logger.LogError
-            (
-                "Failed to edit role menu with guild / channel / message ID {GuildId} / {MessageId} / {ChannelId}",
-                menu.GuildId,
-                menu.ChannelId,
-                menu.MessageId
-            );
-            return editResult;
-        }
+            return await ProcessMessageUpsertErrorResult(menu, upsertResult, ct);
 
-        Result<IMessage> createMsgResult = await _channelApi.CreateMessageAsync
+        upsertResult = await _channelApi.CreateMessageAsync
         (
             DiscordSnowflake.New(menu.ChannelId),
             components: components,
             flags: MessageFlags.IsComponentsV2,
             ct: ct
         );
-        if (createMsgResult.Error is RestResultError<RestError> { Error.Code: { HasValue: true, Value: DiscordError.InvalidFormBody } } createRestError)
-        {
-            // TODO: Drill all the way down, find IErrorDetails.Code = "COMPONENT_INVALID_EMOJI" or error on IPropertyErrorDetails.Key = "emoji"
-            foreach ((string key, OneOf<IPropertyErrorDetails, IReadOnlyList<IErrorDetails>> value) in createRestError.Error.Errors.Value)
-            {
-            }
-        }
 
-        if (createMsgResult.IsDefined(out IMessage? createdMsg))
-            menu.MessageId = createdMsg.ID.Value;
+        if (!upsertResult.IsDefined(out IMessage? createdMsg))
+            return await ProcessMessageUpsertErrorResult(menu, upsertResult, ct);
 
-        return createMsgResult;
+        menu.MessageId = createdMsg.ID.Value;
+        await _dbContext.SaveChangesAsync(ct);
+
+        return upsertResult;
     }
 
-    private async ValueTask<List<IMessageComponent>> CreateRoleMenuMessageComponents
+    private async ValueTask<Result<IMessage>> ProcessMessageUpsertErrorResult
     (
         GuildRoleMenu menu,
+        Result<IMessage> result,
         CancellationToken ct
     )
     {
+        _logger.LogError
+        (
+            "Failed to upsert role menu with guild / channel / message ID {GuildId} / {MessageId} / {ChannelId}",
+            menu.GuildId,
+            menu.ChannelId,
+            menu.MessageId
+        );
+
+        if
+        (
+            result.Error is not RestResultError<RestError>
+            {
+                Error:
+                {
+                    Code: { HasValue: true, Value: DiscordError.InvalidFormBody },
+                    Errors: { HasValue: true, Value: { Count: > 0 } errValues }
+                }
+            }
+        )
+        {
+            return result;
+        }
+
+        // Drill all the way down, only processing the first member error at each level - the user will have to
+        // solve them one at a time anyway
+        (string key, OneOf<IPropertyErrorDetails, IReadOnlyList<IErrorDetails>> value) = errValues.First();
+        if (key is "emoji" || (value.IsT1 && value.AsT1.Any(x => x.Code is "COMPONENT_INVALID_EMOJI")))
+            return await SendEmojiCheckMessage(menu, ct);
+
+        string? currKey = key;
+        IPropertyErrorDetails? currDetails = value.AsT0;
+        do
+        {
+            if (currKey == "emoji" || currDetails.Errors?.Any(x => x.Code is "COMPONENT_INVALID_EMOJI") is true)
+                return await SendEmojiCheckMessage(menu, ct);
+
+            if (currDetails.MemberErrors is not null)
+                (currKey, currDetails) = currDetails.MemberErrors!.FirstOrDefault();
+            else
+                (currKey, currDetails) = (null, null);
+        }
+        while (currDetails is not null);
+
+        return result;
+    }
+
+    private async Task<Result<IMessage>> SendEmojiCheckMessage(GuildRoleMenu menu, CancellationToken ct)
+    {
+        StringBuilder sb = new();
+        sb.AppendLine(Formatter.Bold("The role menu could not be updated due to invalid emoji(s)."));
+        sb.AppendLine
+        (
+            "Please review the below roles on the menu for broken emoji, and use the `add-role` command to update"
+                + " those that are broken"
+        );
+        sb.AppendLine();
+
+        foreach (GuildRoleMenuRole role in menu.Roles.Where(x => x.Emoji is not null))
+            sb.Append(role.Emoji).Append(" - ").AppendLine(Formatter.RoleMention(role.RoleId));
+
+        return await _feedbackService.SendContextualAsync
+        (
+            sb.ToString(),
+            default,
+            new FeedbackMessageOptions
+            (
+                AllowedMentions: new AllowedMentions(Roles: new Optional<IReadOnlyList<Snowflake>>([]))
+            ),
+            ct
+        );
+    }
+
+    private static List<IMessageComponent> CreateRoleMenuMessageComponents(GuildRoleMenu menu)
+    {
         List<SectionComponent> roleButtonsWithDesc = [];
         List<ButtonComponent> roleButtons = [];
-
-        // Validate emoji, otherwise Discord causes issues
-        // foreach (GuildRoleMenuRole role in menu.Roles.Where(x => x.Emoji != null))
-        // {
-        //     Optional<IPartialEmoji> emoji = Formatter.EmojiFromString(role.Emoji!)
-        //         .MapOr(x => new Optional<IPartialEmoji>(x), default);
-        //
-        //     if (!emoji.HasValue || !emoji.Value.ID.HasValue)
-        //         continue;
-        //
-        //     Result<IEmoji> guildEmoji = await _emojiApi.GetGuildEmojiAsync
-        //     (
-        //         DiscordSnowflake.New(menu.GuildId),
-        //         emoji.Value.ID.Value!.Value, ct
-        //     );
-        //
-        //     if (!guildEmoji.IsSuccess)
-        //         role.Emoji = null;
-        // }
 
         foreach (GuildRoleMenuRole role in menu.Roles.Where(x => string.IsNullOrEmpty(x.Description)))
         {
             Optional<IPartialEmoji> emoji = default;
             if (role.Emoji is not null)
+            {
                 emoji = Formatter.EmojiFromString(role.Emoji).MapOr(x => new Optional<IPartialEmoji>(x), default);
+                Console.WriteLine(emoji);
+            }
 
             roleButtons.Add(new ButtonComponent
             (
@@ -182,7 +230,10 @@ public class RoleMenuService : IRoleMenuService
         {
             Optional<IPartialEmoji> emoji = default;
             if (role.Emoji is not null)
+            {
                 emoji = Formatter.EmojiFromString(role.Emoji).MapOr(x => new Optional<IPartialEmoji>(x), default);
+                Console.WriteLine(emoji);
+            }
 
             roleButtonsWithDesc.Add(new SectionComponent
             (
